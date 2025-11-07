f1d7cd0d8b63e78143f5531b1f33cb0dcb22ec9a
Subhankar Dey <sdey1122@gmail.com>
2025-11-07 09:20:10 +0530

    our_product_scetion_responsive

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ searchInput.addEventListener("blur", () => {
   }
 });
 
-// heart Icon Toggle Color
+// Heart Icon Toggle Color
 const heartIcon = document.getElementById("heartIcon");
 
 heartIcon.addEventListener("click", () => {
@@ -21,6 +21,19 @@ heartIcon.addEventListener("click", () => {
   } else {
     heartIcon.classList.remove("fa-solid", "liked");
     heartIcon.classList.add("fa-regular");
+  }
+});
+
+// Heart Icon Toggle Color (Product Page Card)
+const podHeart = document.getElementById("pod-heart");
+
+podHeart.addEventListener("click", () => {
+  if (podHeart.classList.contains("fa-regular")) {
+    podHeart.classList.remove("fa-regular");
+    podHeart.classList.add("fa-solid", "liked");
+  } else {
+    podHeart.classList.remove("fa-solid", "liked");
+    podHeart.classList.add("fa-regular");
   }
 });
 
@@ -36,4 +49,45 @@ navLinks.forEach((link) => {
   } else {
     link.classList.remove("active");
   }
+});
+
+// jQuery (Our Product Scetion)
+function initProductsSlider() {
+  const $slider = $(".autoplay");
+  if (!$slider.hasClass("slick-initialized")) {
+    $slider.slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      prevArrow: $(".left_btn"),
+      nextArrow: $(".right_btn"),
+      responsive: [
+        { breakpoint: 1200, settings: { slidesToShow: 3 } },
+        { breakpoint: 992, settings: { slidesToShow: 3 } },
+        { breakpoint: 860, settings: { slidesToShow: 2 } },
+        { breakpoint: 768, settings: { slidesToShow: 2 } },
+        { breakpoint: 576, settings: { slidesToShow: 1 } },
+      ],
+    });
+  } else {
+    $slider.slick("setPosition");
+  }
+}
+$(function () {
+  $(window).on("load", initProductsSlider);
+});
+
+// Toggle Heart (Our Product Section) (jQuery)
+$(document).on("click", ".prod-heart", function (e) {
+  e.preventDefault();
+  const $btn = $(this);
+  const $icon = $btn.find("i");
+  const pressed = $btn.attr("aria-pressed") === "true";
+  $btn.attr("aria-pressed", !pressed);
+
+  $icon.toggleClass("liked");
+
+  $icon.toggleClass("fa-regular fa-solid");
 });
